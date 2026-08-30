206e874d4daa49792bac6a7c99156caa3385da92
Pranshu <pranshupatel1605@gmail.com>
2026-08-30 10:55:56 +0530

    nltk_update

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,11 @@ import re
 import nltk
 from nltk.corpus import stopwords
 
-nltk.download("stopwords")
-stop_words = set(stopwords.words("english"))
-
+try:
+    stop_words = set(stopwords.words("english"))
+except LookupError:
+    nltk.download("stopwords")
+    stop_words = set(stopwords.words("english"))
 def clean_text(text: str):
     text = text.lower()
     text = re.sub(r"http\S+", "", text)
